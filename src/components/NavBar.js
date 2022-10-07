97ebd79c4057ca7ff283c0dc53451c973401cc09
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-10-07 20:41:35 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
+export const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink class="index-link" to="/">
+            Index Page
+          </NavLink>
+        </li>
 
-export const NavBar = () =>{
-    return(
-
-    )
-}
+        <li>
+          <NavLink class="home-link" to="/home">
+            Home Page
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
